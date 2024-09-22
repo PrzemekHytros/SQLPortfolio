458dc8db4943f091c1b94e05d31a62bb7a3c962a
Przemek Hytroś <przemyslaw.hytros@interia.pl>
2024-09-22 11:29:25 +0200

    productName added

---
+++ SELECT/README.md
@@ -0,0 +1,5 @@
+# SELECT
+
+My SQL projects
+
+Test
---
+++ SELECT/productcName.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Wrz 22, 2024 at 11:27 AM
+-- Wersja serwera: 10.4.32-MariaDB
+-- Wersja PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `w3schools`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `products`
+--
+
+CREATE TABLE `products` (
+  `ProductID` int(11) NOT NULL,
+  `ProductName` varchar(255) DEFAULT NULL,
+  `SupplierID` int(11) DEFAULT NULL,
+  `CategoryID` int(11) DEFAULT NULL,
+  `Unit` varchar(255) DEFAULT NULL,
+  `Price` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ProductName`) VALUES
+('Chais'),
+('Chang'),
+('Aniseed Syrup'),
+('Chef Anton\'s Cajun Seasoning'),
+('Chef Anton\'s Gumbo Mix'),
+('Grandma\'s Boysenberry Spread'),
+('Uncle Bob\'s Organic Dried Pears'),
+('Northwoods Cranberry Sauce'),
+('Mishi Kobe Niku'),
+('Ikura'),
+('Queso Cabrales'),
+('Queso Manchego La Pastora'),
+('Konbu'),
+('Tofu'),
+('Genen Shouyu'),
+('Pavlova'),
+('Alice Mutton'),
+('Carnarvon Tigers'),
+('Teatime Chocolate Biscuits'),
+('Sir Rodney\'s Marmalade'),
+('Sir Rodney\'s Scones'),
+('Gustaf\'s Knäckebröd'),
+('Tunnbröd'),
+('Guaraná Fantástica'),
+('NuNuCa Nuß-Nougat-Creme');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ProductID`),
+  ADD KEY `CategoryID` (`CategoryID`),
+  ADD KEY `SupplierID` (`SupplierID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ProductID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`CategoryID`) REFERENCES `categories` (`CategoryID`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`SupplierID`) REFERENCES `suppliers` (`SupplierID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
